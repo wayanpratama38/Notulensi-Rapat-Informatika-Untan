07f73dc829116e2e6d3da789e93c78cc85313fa9
wynprtm <wayanpratama38@gmail.com>
2025-04-30 01:23:22 +0700

    Adding add-meeting page and handling routing into add-meeting page

---
+++ src/app/(add-meeting)/add-meeting/page.js
@@ -0,0 +1,5 @@
+export default function AddMeeting() {
+    return (
+        <div>ADD MEETING PAGE</div>
+    )
+}
--- src/components/ui/tab-panel-content.js
+++ src/components/ui/tab-panel-content.js
@@ -10,7 +10,7 @@ export default function TabPanelContent({meetings=[]}) {
         <TabPanel className="w-full min-h-[30rem] mb-50 p-4 sm:p-6 md:pd-8 bg-white rounded-lg  transition-all duration-all border border-gray-100 shadow-md">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 cursor-pointer">
                 {meetings.map((meeting, index) => (
-                    <Link href={`meeting/${meeting.id}`}>
+                    <Link key={meeting.id} href={`meeting/${meeting.id}`}>
                         <div 
                         key={index}
                         onClick={()=>console.log(meeting.id)}
