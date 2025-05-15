a3d4bfbf9a7277e848b928865abece724903c9bb
wynprtm <wayanpratama38@gmail.com>
2025-05-15 14:04:54 +0700

    Changing root page in (admin)

--- src/app/(admin)/page.js
+++ src/app/(admin)/page.js
@@ -1,5 +1,5 @@
-import {redirect} from "next/navigation";
+import { redirect } from "next/navigation";
 
-export default function AdminRootPage() { 
-    redirect("/")
+export default function AdminRootPage() {
+  return redirect("/dashboard");
 }
